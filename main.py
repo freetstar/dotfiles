7301c8dd5678fc996b59b1ec47d9ac56b9b85e8d
freetstar <lgxwqq@gmail.com>
2012-04-09 14:47:38 +0800

    fix

--- main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from tomate import ui
-
-if __name__ == '__main__':
-    ui.start_app()
--- runtests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tests=`(cd tests; ls *.py | grep -v __init__)`
-for test in $tests; do
-    echo "Running test: $test"
-    python -m tests.${test%.py}
-done
